671ae5e9c99e548e1641475b9c8ee4fa88b999b9
Dylan Stewart <dylanstewart0824@gmail.com>
2023-02-20 07:48:07 -0500

    Work in Progress of Connection handling, posting to DB, and making code and console more pretty

--- pico_files/Clim_Sensor.py
+++ pico_files/Clim_Sensor.py
@@ -13,47 +13,57 @@ i2c0 = I2C(0, sda=i2c0_sda, scl=i2c0_scl)
 
 dht20 = DHT20(0x38, i2c0)
 
-#Getting MAC Address
+#Getting MAC Address, hostname, and IP
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
-print(mac)
+hostname = network.WLAN().config('hostname')
+ip = network.WLAN().config('ip')
+print(f"My MAC address is: {mac}")
+print(f"My hostname is: {hostname}")
+print(f"My hostname is: {ip}")
+
 
 #Connect to the internet
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
 wlan.connect(ssid='ut-open')
-print(wlan.isconnected())
+if wlan.isconnected():
+    print("Pico is online")
+
+else:
+    print("Pico could not connect to the internet!")
+    reboot = input("Reboot Pico? (y/n): ")
+    if reboot == y:
+        machine.reset()
 
 
 #Some ideas on how to set variables necessary to communicate with influxDB on SERF server
-'''
-INFLUXDB_URL = ""
-INFLUXDB_TOKEN = ""
-INFLUXDB_ORG = ""
-INFLUXDB_BUCKET =""
-TimeZone_INFO = ""
-
-OR 
-
-# Set the databasename,location name
-database = "temp/humd"
+# Set the location name
 location = "SERF_212"
 
 # Set the URL for the influxDB 2.0 HTTP API
-url = f"http://serf212a:port/write?db={database}"
+url = "http://localhost:8086/api/v2/write?bucket=db/rp&precision=ns"
 
 # Set the authentication header
-auth_header = f"Token {credentials.influxUser}:{credentials.influxPW}"
-'''
+auth_header = f"Token {username}:{password}"
 
-# Checking the temp and humidity each second and posting that to DB
+#Set the refresh rate
+refresh = 10
+
+# Checking the temp and humidity every 10s and posting that to DB
 while True:
+    if not wlan.isconnected():
+        print("Pico has lost connection!")
+        machine.reset()
+        break
+
     measurements = dht20.measurements
     status = f"In {location}, Temperature: {measurements['t']} °C, humidity: {measurements['rh']} %RH"
     print(status)
-    '''
+
+    data = f"climate,location={location} temperature={measurements['t']},humidity={measurements['rh']}"
+
     # Use the requests library to send a POST request to the influxDB
+
     response = requests.post(
         url=url,
         headers={"Authorization": auth_header},
@@ -63,5 +73,5 @@ while True:
     print(response.status_code)
 
     # Code 204 = ok
-    '''
-    sleep(1)
+    
+    sleep(refresh)
